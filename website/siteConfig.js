533bd688a42b2477ce0ef5ad62019bb2396a791e
Justin Headley <>
2018-06-06 01:09:52 -0500

    - Added some initial docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,24 +21,24 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site' /* title for your website */,
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  title: 'rest-hapi' /* title for your website */,
+  tagline: 'A RESTful API generator ',
+  url: 'https://jkheadley.github.io' /* your website url */,
+  baseUrl: '/rest-hapi/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'rest-hapi',
+  organizationName: 'JKHeadley',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
+    {doc: 'getting-started', label: 'Docs'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
@@ -74,7 +74,7 @@ const siteConfig = {
   copyright:
     'Copyright © ' +
     new Date().getFullYear() +
-    ' Your Name or Your Company Name',
+    ' Justin Headley',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
